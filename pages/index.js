5e9ca0352876331769fddc90c64396b4195f9295
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-12-16 17:16:41 -0300

    Pré-renderização e busca de dados

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +23,22 @@ export default function Home() {
         <p>
           Espaço onde compartilho novos conhecimentos em <code>desenvolvimento web!</code>
         </p>
+      </section>
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
